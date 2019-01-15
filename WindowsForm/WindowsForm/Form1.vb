c4b02ff701e456f3ede3588fea2edce0aa34e683
Tanawat chinprapinporn <tanawatz@hotmail.com>
2019-01-15 15:10:57 +0700

    Completed

---
+++ WindowsForm/WindowsForm/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim pro As New Product("P-001", "Pen", 200, 12.5)
+        'pro.Id = "P-001"
+        'pro.Name = "Pen"
+        'pro.Item = 200
+        'pro.Price = 12.5
+        Dim message As String = "รหัสสินค้า: " & pro.Id & "(" & pro.Name & ")" & vbNewLine &
+                                "จำนวนชิ้น: " & pro.Item & ", ราคาต่อขิ้น: " & pro.Price &
+                                vbNewLine & "ราคารวมทั้งหมด: " & pro.calculate() & " บาท"
+
+        MessageBox.Show(message)
+    End Sub
+End Class
---
+++ WindowsForm/WindowsForm/Product.vb
@@ -0,0 +1,73 @@
+﻿Public Class Product
+    Private _id, _name As String
+    Private _item As Integer
+    Private _price As Double
+
+    Public Sub New()
+        _id = "P-000"
+        _name = "XXX"
+        _item = 999
+        _price = 999
+    End Sub
+
+    Public Sub New(id As String, name As String, item As String,
+                   price As Double)
+        _id = id
+        _name = name
+        _item = item
+        _price = price
+
+    End Sub
+
+    Public Property Id As String
+        Get
+            Return _id
+        End Get
+        Set(value As String)
+            _id = value
+        End Set
+    End Property
+
+    Public Property Name As String
+        Get
+            Return _name
+        End Get
+        Set(value As String)
+            _name = value
+        End Set
+    End Property
+
+    Public Property Item As Integer
+        Get
+            Return _item
+        End Get
+        Set(value As Integer)
+            _item = value
+        End Set
+    End Property
+
+    Public Property Price As Double
+        Get
+            Return _price
+        End Get
+        Set(value As Double)
+            _price = value
+        End Set
+    End Property
+
+    Public Function checkItem() As Boolean
+        Return _item <= 0
+        Return False
+    End Function
+
+    Public Function checkPrice() As Boolean
+        Return _price <= 0
+        Return False
+    End Function
+
+    Public Function calculate() As Double
+        Return _item * _price
+        Return False
+    End Function
+
+End Class
